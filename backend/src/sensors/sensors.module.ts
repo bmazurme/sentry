67faae620494b84131de98d8320146059645501c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-07-03 12:04:09 +0300

    add: SensorsModule

---
+++ backend/src/sensors/sensors.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Sensor } from './sensor.entity';
+import { SensorsController } from './sensors.controller';
+import { SensorsService } from './sensors.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Sensor])],
+  controllers: [SensorsController],
+  providers: [SensorsService],
+  exports: [SensorsService],
+})
+export class SensorsModule {}

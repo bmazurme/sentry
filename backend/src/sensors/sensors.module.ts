39107dc3ac8b73ed04a5fa1a59750e3377dcf0ce
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2026-07-09 22:20:09 +0300

    fix: SensorsModule

--- backend/src/sensors/sensors.module.ts
+++ backend/src/sensors/sensors.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Sensor } from './sensor.entity';
 import { SensorsController } from './sensors.controller';
 import { SensorsService } from './sensors.service';
+import { MqttModule } from '../mqtt/mqtt.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sensor])],
+  imports: [TypeOrmModule.forFeature([Sensor]), MqttModule],
   controllers: [SensorsController],
   providers: [SensorsService],
   exports: [SensorsService],
